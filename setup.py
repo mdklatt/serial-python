09b6369b841d3154b5c6d9e0ac2f149a2ee582a3
Michael Klatt <mdklatt@ou.edu>
2013-05-10 09:49:19 -0500

    added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,66 @@
-""" Distutils setup script for the serial.core library package.
+""" Distutils setup script for the serial.core library.
 
-For basic installation in the current user's site-packages directory:
-    python setup.py install --user
+Basic command to run the test suite and install library in the user's package
+directory if all tests pass:
+
+    python setup.py test install --user
 
 """
+from distutils.core import Command
 from distutils.core import setup
 from sys import exit
 
 from serial.core import __version__
+from test import run as run_tests
 
+SETUP_CONFIG = {
+    "name": "serial-core",
+    "packages": ("serial", "serial.core"),
+    "author": "Michael Klatt",
+    "author_email": "mdklatt@ou.edu",
+    "version": __version__}
+
+
+class TestCommand(Command):
+    """ Custom setup command to run the test suite.
+    
+    """
+    description = "run the test suite"
+    user_options = []
+    
+    def initialize_options(self):
+        """ Set the default values for all options this command supports.
+        
+        """
+        return
+        
+    def finalize_options(self):
+        """ Set final values for all options this command supports.
+        
+        This is run after all other option assigments have been completed (e.g.
+        command-line options, other commands, etc.)
+        
+        """
+        return
+
+    def run(self):
+        """ Execute the command.
+        
+        """
+        if run_tests() != 0:
+            raise RuntimeError("test suite failed")
+        return
+        
 
 def main():
-    config = {
-        "name": "serial-core",
-        "version": __version__,
-        "packages": ("serial", "serial.core"),
-        "author": "Michael Klatt",
-        "author_email": "mdklatt@ou.edu"}
-    setup(**config)
+    """ Execute the setup commands.
+    
+    """
+    setup(cmdclass={"test": TestCommand}, **SETUP_CONFIG)
     return 0
 
+
+# Make the script executable.
 
 if __name__ == "__main__":
     exit(main())
