cd303394df148c4627f2a578536cb4fb94566c0e
Michael Klatt <mdklatt@ou.edu>
2013-05-17 18:04:32 -0500

    added IStreamZlib adaptor

--- serial/core/stream.py
+++ serial/core/stream.py
@@ -1,7 +1,10 @@
 """ Tools for working with streams.
 
 """
-__all__ = ("IStreamAdaptor", "IStreamBuffer", "OStreamAdaptor")
+from zlib import decompressobj
+from zlib import MAX_WBITS
+
+__all__ = ("IStreamAdaptor", "OStreamAdaptor", "IStreamBuffer", "IStreamZlib")
 
 
 class IStreamAdaptor(object):
@@ -26,8 +29,23 @@ class IStreamAdaptor(object):
         return self
 
 
+class OStreamAdaptor(object):
+    """ Abstract base class for an output stream adaptor.
+
+    An adaptor can be used to make an output source compatible with the Writer
+    stream protocol, i.e. implementing a write() method that writes a single
+    line of text to the stream.
+
+    """
+    def write(self):
+        """ Write a line of text to the stream.
+
+        """
+        raise NotImplementedError
+
+
 class IStreamBuffer(IStreamAdaptor):
-    """ Add buffering to a stream.
+    """ Add buffering to an input stream.
 
     An IStreamBuffer buffers input from another stream so that it can support
     rewind() operations.
@@ -78,16 +96,58 @@ class IStreamBuffer(IStreamAdaptor):
         return
 
 
-class OStreamAdaptor(object):
-    """ Abstract base class for an output stream adaptor.
-
-    An adaptor can be used to make an output source compatible with the Writer
-    stream protocol, i.e. implementing a write() method that writes a single
-    line of text to the stream.
-
-    """
-    def write(self):
-        """ Write a line of text to the stream.
-
+class IStreamZlib(IStreamAdaptor):
+    """ Add zlib decompression to an input stream.
+    
+    This adaptor can be used with any zlib-compressed data (includes gzip).
+    Unlike the Python gzip module, this *will* work with network streams e.g. a 
+    urlopen() stream.
+    
+    """  
+    # Adjust the block size to trade performance against memory usage. 
+    block_size = 32*1024  # bytes; must be at least 4 bytes.
+      
+    def __init__(self, stream):
+        """ Initialize this object.
+        
+        The input stream must implement a read() method that returns a user-
+        specified number of bytes, c.f. a Python file object.
+        
         """
-        raise NotImplementedError
+        super(IStreamZlib, self).__init__()
+        self._stream = stream
+        wbits = MAX_WBITS + 32  # <http://www.zlib.net/manual.html#Advanced>
+        self._zlib = decompressobj(wbits)
+        self._buffer = []
+        return
+            
+    def next(self):
+        """ Return the next line of text.
+        
+        """
+        while True:
+            # Find the end of the next complete line.
+            try:
+                pos = self._buffer.index("\n") + 1  # include newline in line
+            except ValueError:  # index failed
+                # Keep going as long as the stream is still good, otherwise
+                # this is the last line (the newline is missing).
+                if self._read():
+                    continue
+                pos = len(self._buffer) 
+            break
+        if not self._buffer:
+            raise StopIteration
+        line = "".join(self._buffer[:pos])
+        self._buffer = self._buffer[pos:]
+        return line
+        
+    def _read(self):
+        """ Retrieve decompressed data from the stream.
+        
+        """
+        # The block size is based on the compressed data; the returned data
+        # size may be different.
+        data = self._zlib.decompress(self._stream.read(self.block_size))
+        self._buffer.extend(list(data))
+        return len(data) > 0
--- test/test_stream.py
+++ test/test_stream.py
@@ -4,11 +4,14 @@ The module can be executed on its own or incorporated into a larger test suite.
 
 """
 from StringIO import StringIO
+from io import BytesIO
+from zlib import compress
 
 import _path
 import _unittest as unittest
 
 from serial.core import IStreamBuffer
+from serial.core import IStreamZlib
 
 
 # Define the TestCase classes for this module. Each public component of the
@@ -26,8 +29,9 @@ class IStreamBufferTest(unittest.TestCase):
 
         """
         self.bufsize = 2
-        self.buffer = IStreamBuffer(StringIO("abc\ndef\nghi\n"), self.bufsize)
         self.lines = ("abc\n", "def\n", "ghi\n")
+        stream = StringIO("".join(self.lines))
+        self.stream = IStreamBuffer(stream, self.bufsize)
         return
 
     def test_iter(self):
@@ -36,25 +40,59 @@ class IStreamBufferTest(unittest.TestCase):
         Tests both __iter__() and next().
 
         """
-        self.assertSequenceEqual(self.lines, list(self.buffer))
+        self.assertSequenceEqual(self.lines, list(self.stream))
         return
         
     def test_rewind(self):
         """ Test the rewind() method.
         
         """
-        self.buffer.next()
-        self.buffer.rewind()  # back to first line
-        self.assertSequenceEqual(self.lines, list(self.buffer))
-        self.buffer.rewind(100)  # try to advance past beginning of buffer
-        self.assertSequenceEqual(self.lines[-self.bufsize:], list(self.buffer))
-        self.assertSequenceEqual([], list(self.buffer))  # stream is exhausted
+        self.stream.next()
+        self.stream.rewind()  # back to first line
+        self.assertSequenceEqual(self.lines, list(self.stream))
+        self.stream.rewind(100)  # try to advance past beginning of buffer
+        self.assertSequenceEqual(self.lines[-self.bufsize:], list(self.stream))
+        self.assertSequenceEqual([], list(self.stream))  # stream is exhausted
         return
 
+class IStreamZlibTest(unittest.TestCase):
+    """ Unit testing for the IStreamZlib class.
+
+    """
+    def setUp(self):
+        """ Set up the test fixture.
+
+        This is called before each test is run so that they are isolated from
+        any side effects. This is part of the unittest API.
+
+        """
+        self.lines = ("abcd\n", "efgh\n", "ijkl")  # test with missing newline
+        self.stream = IStreamZlib(BytesIO(compress("".join(self.lines))))
+        return
+
+    def test_iter_zlib(self):
+        """ Test the iterator protocol.
+    
+        Tests both __iter__() and next().
+    
+        """
+        self.assertSequenceEqual(self.lines, list(self.stream))
+        return
+
+    def test_iter_long(self):
+        """ Test the iterator protocol for long lines.
+
+        Tests both __iter__() and next().
+
+        """
+        # Test for lines longer than than the block size.
+        self.stream.block_size = 4  # minimum block size
+        self.assertSequenceEqual(self.lines, list(self.stream))
+        return
 
 # Specify the test cases to run for this module (disables automatic discovery).
 
-_TEST_CASES = (IStreamBufferTest,)
+_TEST_CASES = (IStreamBufferTest, IStreamZlibTest)
 
 def load_tests(loader, tests, pattern):
     """ Define a TestSuite for this module.
