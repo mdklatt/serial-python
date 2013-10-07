c2291e741d687eeca32225cbe3d8472065d7aed4
Michael Klatt <mdklatt@ou.edu>
2013-10-07 15:15:31 -0500

    added TextFilter class

--- serial/core/filter.py
+++ serial/core/filter.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 
 from re import compile
 
-__all__ = ("FieldFilter",)
+__all__ = ("FieldFilter", "TextFilter")
 
 
 class FieldFilter(object):
@@ -19,7 +19,7 @@ class FieldFilter(object):
         
         By default, records that match one of the given field values are passed
         through and all other records are dropped (whitelisting). If whitelist
-        if False this is reversed (blacklisting).
+        is False this is reversed (blacklisting).
         
         """
         self._field = field
@@ -33,3 +33,30 @@ class FieldFilter(object):
         """
         valid = (record[self._field] in self._values) == self._whitelist
         return record if valid else None
+
+
+class TextFilter(object):
+    """ Filter lines using a regular expression.
+    
+    This is intended for use with an IStreamFilter.
+    
+    """
+    def __init__(self, regex, whitelist=True):
+        """ Initialize this object.
+        
+        By default, records that match the regular expression are passed 
+        through and all other records are dropped (whitelisting). If whitelist
+        is False this is reversed (blacklisting).
+        
+        """
+        self._regex = compile(regex)
+        self._whitelist = whitelist
+        return
+        
+    def __call__(self, line):
+        """ Execute the filter.
+        
+        """
+        valid = (self._regex.search(line) is not None) == self._whitelist
+        return line if valid else None
+                
--- test/test_filter.py
+++ test/test_filter.py
@@ -7,11 +7,56 @@ import _path
 import _unittest as unittest
 
 from serial.core import FieldFilter
+from serial.core import TextFilter
 
 # Define the TestCase classes for this module. Each public component of the
 # module being tested has its own TestCase.
 
-class FieldFilterTest(unittest.TestCase):
+
+class _FilterTest(unittest.TestCase):
+    """ Unit testing for filter classes.
+
+    This is an abstract class and should not be called directly by any test
+    runners.
+
+    """
+    def test_call(self):
+        """ Test the __call__ method.
+        
+        """ 
+        filtered = self.data[:2] + [None]
+        self.assertSequenceEqual(filtered, map(self.whitelist, self.data))
+        return
+    
+    def test_call_blacklist(self):
+        """ Test the __call__ method for blacklisting.
+        
+        """ 
+        filtered = [None]*2 + self.data[2:]
+        self.assertSequenceEqual(filtered, map(self.blacklist, self.data))
+        return
+
+
+class FieldFilterTest(_FilterTest):
+    """ Unit testing for the FieldFilter class.
+
+    """
+    def setUp(self):
+        """ Set up the test fixture.
+
+        This is called before each test is run so that they are isolated from
+        any side effects. This is part of the unittest API.
+
+        """
+        # Filters need to match the first two records.
+        values = ("abc", "def", "ghi")
+        self.whitelist = FieldFilter("test", values[:2])
+        self.blacklist = FieldFilter("test", values[:2], False)
+        self.data = [{"test": value} for value in values]
+        return
+
+                
+class TextFilterTest(_FilterTest):
     """ Unit testing for the WhitelistFilter class.
 
     """
@@ -22,31 +67,17 @@ class FieldFilterTest(unittest.TestCase):
         any side effects. This is part of the unittest API.
 
         """
-        self.records = ({"field": "abc"}, {"field": "def"}, {"field": "ghi"})
+        # Filters need to match the first two lines.
+        regex = r"abc|def"
+        self.whitelist = TextFilter(regex)
+        self.blacklist = TextFilter(regex, False)
+        self.data = ["abc\n", "def\n", "ghi\n"]
         return
-        
-    def test_call(self):
-        """ Test the __call__ method.
-        
-        """
-        whitelist = FieldFilter("field", ("abc", "def"))
-        filtered = (self.records[0], self.records[1], None)
-        self.assertSequenceEqual(filtered, map(whitelist, self.records))
-        return
-        
-    def test_call_blacklist(self):
-        """ Test the __call__ method with blacklisting.
-        
-        """
-        blacklist = FieldFilter("field", ("abc", "def"), False)
-        filtered = (None, None, self.records[2])
-        self.assertSequenceEqual(filtered, map(blacklist, self.records))
-        return
-        
 
+        
 # Specify the test cases to run for this module (disables automatic discovery).
 
-_TEST_CASES = (FieldFilterTest,)
+_TEST_CASES = (FieldFilterTest, TextFilterTest)
 
 def load_tests(loader, tests, pattern):
     """ Define a TestSuite for this module.
