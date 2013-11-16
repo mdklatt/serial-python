0da58ff4b1348adb745409bb4e5a9c0ee24e54e8
Michael Klatt <mdklatt@ou.edu>
2013-11-16 01:40:25 -0600

    added update command to setup.py [ci skip]

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ if all tests pass:
 """
 from distutils.core import Command
 from distutils.core import setup
+from subprocess import check_call
+from tempfile import TemporaryFile
 
 from serial.core import __version__
 from test import run as run_tests
@@ -49,13 +51,69 @@ class TestCommand(Command):
         if run_tests() != 0:
             raise RuntimeError("test suite failed")
         return
+
+
+class UpdateCommand(Command):
+    """ Custom setup command to update from the tracking branch.
+    
+    """
+    description = "update from the tracking branch"
+    user_options = [
+        ("remote=", "r", "remote name [default: tracking remote]"),
+        ("branch=", "b", "branch name [default: tracking branch]")]
+    
+    def initialize_options(self):
+        """ Set the default values for all options this command supports.
         
+        """
+        # By default, use the remote tracking branch.
+        self.remote = ""
+        self.branch = ""
+        return
+        
+    def finalize_options(self):
+        """ Set final values for all options this command supports.
+        
+        This is run after all other option assigments have been completed (e.g.
+        command-line options, other commands, etc.)
+        
+        """
+        return
+
+    def run(self):
+        """ Execute the command.
+        
+        """
+        args = {"remote": self.remote, "branch": self.branch}
+        cmdl = "git pull --ff-only {remote:s} {branch:s}".format(**args)
+        check_call(cmdl.split())  # throws exception on error
+        return
+
+
+# class Git(object):
+#     """ Basic git interface.
+#     
+#     """
+#     
+#     
+#     def _exec(self, cmd, *args):
+#         """ Execute a git command.
+#         
+#         The return value is the output send to stdout. If the command returns
+#         with an error a CalledProcessError exception is raised.
+#         
+#         """
+        
+    
+
+    
 
 def main():
     """ Execute the setup commands.
     
     """
-    setup(cmdclass={"test": TestCommand}, **SETUP_CONFIG)
+    SETUP_CONFIG["cmdclass"] = {"test": TestCommand, "update": UpdateCommand}
+    setup(**SETUP_CONFIG)
     return 0
 
 
