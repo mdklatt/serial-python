026b7efe00794d9b447447bf648f16071f509f23
Michael Klatt <mdklatt@ou.edu>
2015-06-07 14:12:22 -0500

    __version__ complies with PEP 0440

--- serial/core/__version__.py
+++ serial/core/__version__.py
@@ -1,8 +1,11 @@
 """ The current version of the serial.core package.
 
-This package uses the Semantic Versioning scheme:
+This package uses the Semantic Versioning scheme in conjunction with PEP 0440:
+
     <http://semver.org/>
-    
+    <https://www.python.org/dev/peps/pep-0440>
+
+
 Major versions introduce significant changes to the API, and backwards 
 compatibility is not guaranteed. Minor versions are for new features and other
 backwards-compatible changes to the API. Patch versions are for bug fixes and
@@ -12,4 +15,4 @@ Version 0.x should be considered a development version with an unstable API,
 and backwards compatibility is not guaranteed for minor versions.
 
 """
-__version__ = "0.5.0"
+__version__ = "0.5.1.dev0"
